bbe6512000af358ef25321404d3c583eeb639e58
Ethan <theethancastro@gmail.com>
2022-10-10 12:44:30 -0400

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,1 +1,173 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Azzemble | Home</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/Logo.png">
+    <!-- Remix icons -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- Swiper.js styles -->
+    <link rel="stylesheet" href="./assets/css/swiper-bundle.min.css"/>
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+<body>
+    <!-- Header -->
+    <header class="header" id="header">
+        <nav class="navbar container">
+            <a href="./index.html">
+                <h2 class="logo">Azzemble</h2>
+            </a>
+            <div class="menu" id="menu">
+                <ul class="list">
+                    <li class="list-item">
+                        <a href="./index.html" class="list-link current">Home</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="./Insights.html" class="list-link">Insights</a>
+                    </li>
+                     <li class="list-item">
+                        <a href="./GenZ.html" class="list-link">Gen Z</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="./Passions.html" class="list-link">Passions</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="./Informative.html" class="list-link">Informative</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="./projects.html" class="list-link">Projects</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="./authors.html" class="list-link">Authors</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="./about.html" class="list-link">About</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdMMxMuK40kJ5iflMZSzWbC8u7ZPETJ2cgGv07ULt8b0iQbGg/viewform?usp=sf_link" class="list-link">Write</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="./contact.html" class="list-link">Contact</a>
+                    </li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                    <li><a href="login.php">Log In</a></li>
+                   
+                   
+                    
+                </ul>
+            </div>
+            <div class="list list-right">
+                <button class="btn place-items-center" id="theme-toggle-btn">
+                    <i class="ri-sun-line sun-icon"></i>
+                    <i class="ri-moon-line moon-icon"></i>
+                </button>
+                    
+               <div class="container">
+                   
+                   
+              
+                <button class="btn place-items-center" id="search-icon">
+                    <i class="ri-search-line"></i>
+                </button>
+                <button class="btn place-items-center screen-lg-hidden menu-toggle-icon" id="menu-toggle-icon">
+                    <i class="ri-menu-3-line open-menu-icon"></i>
+                    <i class="ri-close-line close-menu-icon"></i>
+                </button>
+               
+                </a>
+                 
+            </div>
+        </nav>
+    </header>
+  
+  
+  
+  
+    <section class="signup-form">
+      <h2>Sign Up</h2>
+      <form action="signup.inc.php" method="post">
+       <input type="text" name="name" placeholder="Full name...">
+       <input type="text" name="email" placeholder="Email...">
+       <input type="text" name="uid" placeholder="Username...">
+       <input type="password" name="pwd" placeholder="Password...">
+       <input type="password" name="pwdrepeat" placeholder="Repeat Password...">
+        <button type="submit" name="submit">Sign Up</button>
+      </form>
+        </div>
+        </section>
+      
+      
+      
+      
+      
+      
+      
+    <!-- Newsletter -->
+    <section class="newsletter section">
+        <div class="container">
+            <h2 class="title section-title" data-name="Newsletter">Newsletter</h2>
+            <div class="form-container-inner">
+                <h6 class="title newsletter-title">Subscribe to Azzemble</h6>
+                <p class="newsletter-description">See what Gen(ius) Z has to say.</p>
+              
+              
+                    <button class="btn form-btn" type="submit">
+                    <a href="https://forms.gle/pcNmuz4Gajs7RjRRA" class="list-link">
+                        <i class="ri-mail-send-line"></i>
+                        </a>
+                      
+                    </button>
+            </div>
+        </div>
+    </section>
+    <!-- Footer -->
+    <footer class="footer section">
+        <div class="footer-container container d-grid">
+            
+            <div class="company-data">
+                <a href="./index.html">
+                    <h2 class="logo">Azzemble</h2>
+                </a>
+                <p class="company-description">Working on creating my own blog site, pivoting my writing from other platforms to this one, then scaling.
+End goal: Assemble the greatest minds of Gen Z to help LiBerate (Lift up and Build upon each other).</p>
+                <ul class="list social-media">
+                    <li class="list-item">
+                        <a href="https://www.instagram.com/ethan.a.castro/" class="list-link"><i class="ri-instagram-line"></i></a>
+                    </li>
+                  
+                    <li class="list-item">
+                        <a href="https://twitter.com/EthanCastr" class="list-link"><i class="ri-twitter-line"></i></a>
+                    </li>
+                   
+                </ul>
+                <span class="copyright-notice">&copy;2022 Azzemble. All rights reserved.</span>
+            </div>
+           
+           
+            <div>
+                <h6 class="title footer-title">Company</h6>
+                <ul class="footer-list list">
+                    <li class="list-item">
+                        <a href="./contact.html" class="list-link">Contact us</a>
+                    </li>
+                
+                    <li class="list-item">
+                        <a href="./authors.html" class="list-link">Authors</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdMMxMuK40kJ5iflMZSzWbC8u7ZPETJ2cgGv07ULt8b0iQbGg/viewform?usp=sf_link" class="list-link">Write</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <!-- Swiper.js -->
+    <script src="./assets/js/swiper-bundle.min.js"></script>
+    <!-- Custom script -->
+    <script src="./assets/js/main.js"></script>
+</body>
+</html>
 
