6fdc3296f65426be387f75938c977e29f1ae790f
Ethan <theethancastro@gmail.com>
2022-10-10 12:43:50 -0400

    Update dbh.Inc.php

--- includes/dbh.Inc.php
+++ includes/dbh.Inc.php
@@ -1,1 +1,12 @@
+<?php
 
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "Azzemble login";
+
+$conn = msqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
